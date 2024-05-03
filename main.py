e37e794ae63cdf55b13b03fc3ac2ac6da9f07934
Marius(Laptop) <schaeublemarius@gmail.com>
2024-05-03 17:02:30 +0200

    fast fertig mit einem teil pr, arrayfun probleme

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import sympy as sym
 from Helpers import identifier, isCharacter
 import math
-from numpy import matrix, array, mean, std, max, linspace, ones, sin, cos, tan, arctan, pi, sqrt, exp
+from numpy import matrix, array, mean, std, max, linspace, ones, sin, cos, tan, arctan, pi, sqrt, exp, arcsin, arccos, arctan2, sinh, cosh
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import plot, show, xlabel, ylabel, legend, title, savefig, errorbar, grid
 import scipy.optimize as opt
